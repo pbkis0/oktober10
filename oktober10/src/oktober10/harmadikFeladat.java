b69fa4153922f0ca69a71019896f963e9a664f9f
Balázs Kis <kis.balazs2014@outlook.com>
2018-10-10 18:42:32 +0200

    harmadikFeladat

---
+++ oktober10/src/oktober10/harmadikFeladat.java
@@ -0,0 +1,12 @@
+package oktober10;
+
+public class harmadikFeladat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*Módosítsuk az elozo programot úgy, hogy ne az 1 és 20 közé eso páros,
+hanem a paraméterként megadott két szám közé eso 3-mal osztható
+számokat listázzuk ki! */
+	}
+
+}

52132dd6e07051ad786497ff2b096a6fcacf07c9
Balázs Kis <kis.balazs2014@outlook.com>
2018-10-10 20:10:16 +0200

    kilencedikFeladat -> Dávid

--- oktober10/src/oktober10/kilencedikFeladat.java
+++ oktober10/src/oktober10/kilencedikFeladat.java
@@ -7,6 +7,17 @@ public class kilencedikFeladat {
 /*Írj programot, amely kirajzolja az amerikai zászlót! (A bal felso sarokban
 csillagok, a többi részen kötojelek.) A megoldáshoz használj egymásba
 ágyazott ciklusokat! */
+		
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 40; j++) {
+				if(i < 5 && j < 15) {
+					System.out.println("*");
+				} else {
+					System.out.println("-");				}
+			}
+		}
+		System.out.println();
+		
 	}
-
+}
 }

7a97ad5fb2e7516e12b1d99dfe1fcab0c7a57525
Balázs Kis <kis.balazs2014@outlook.com>
2018-10-10 20:04:04 +0200

    hetedikFeladat

--- oktober10/src/oktober10/hetedikFeladat.java
+++ oktober10/src/oktober10/hetedikFeladat.java
@@ -6,6 +6,18 @@ public class hetedikFeladat {
 		// TODO Auto-generated method stub
 /*Készíts programot, amely a paraméterben megadott tetszoleges számú
 paraméternek számítja ki a számtani közepét!*/
+		
+		
+		if(args.length > 0) {
+			int osszeg = 0;
+			for (int i = 0; i < args.length; i++) {
+				int szam = Integer.parseInt(args[i]);
+				osszeg += szam;
+			}
+			System.out.println((double) osszeg / args.length);
+		}
+		
+		
 	}
 
 }

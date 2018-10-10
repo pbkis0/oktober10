38803e21c61e286e59936befc6447688f2798c82
Balázs Kis <kis.balazs2014@outlook.com>
2018-10-10 19:38:33 +0200

    otodikFeladat ->Dávid

--- oktober10/src/oktober10/otodikFeladat.java
+++ oktober10/src/oktober10/otodikFeladat.java
@@ -7,6 +7,20 @@ public class otodikFeladat {
 /* Írj programot, mely két paraméterrel muködik. Az elso egy karakterlánc, a
 második pedig egy szám, amely azt adja meg, hányszor íródjon ki
 lépcsozetesen a képernyore az elso paraméter.*/
+		if(args.length == 2) {
+			String szo = args[0];
+			int szam = Integer.parseInt(args[1]);
+			
+			for (int i = 0; i <szam; i++) {
+				for (int j=0; j<i; j++) {
+					System.out.print(" ");
+				}
+				System.out.println(szo);
+			}
+		} else {
+			System.err.println("Két paramétert várok!");
+		}
+		
 	}
 
 }

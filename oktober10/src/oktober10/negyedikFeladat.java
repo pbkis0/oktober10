4ae2d6ef7c7267ae0ac7912b8666fc985226e20e
Balázs Kis <kis.balazs2014@outlook.com>
2018-10-10 19:32:57 +0200

    negyedikFeladat -> Dávid

--- oktober10/src/oktober10/negyedikFeladat.java
+++ oktober10/src/oktober10/negyedikFeladat.java
@@ -7,6 +7,25 @@ public class negyedikFeladat {
 /*Írj olyan programot, amely kiírja a paraméterként megadott szám osztóit!
 Ha a szám prím, errol is tájékoztassa a felhasználót! A megoldáshoz
 használj for-ciklust! */
+		if(args.length == 1) {
+			int szam = Integer.parseInt(args[0]);
+			int oszto = 0;
+			
+			System.out.println(szam + " osztói: ");
+			for (int i = 1; i <= szam; i++) {
+				if(szam % i == 0) {
+					System.out.println(i + " ");
+					oszto++;
+				}
+			}
+			if(oszto == 2) {
+				System.out.println("A szám prímszám!");
+			} else {
+				System.out.println("A szám !prímszám.");
+			}
+		}
+		
+		
 	}
 
 }

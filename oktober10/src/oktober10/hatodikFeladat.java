f060c9de11e2203dd1a6583051377e512cce3401
Balázs Kis <kis.balazs2014@outlook.com>
2018-10-10 19:53:57 +0200

    hatodikFeladat -> Dávid

--- oktober10/src/oktober10/hatodikFeladat.java
+++ oktober10/src/oktober10/hatodikFeladat.java
@@ -10,6 +10,24 @@ Segítség: egy szám tökéletes, ha a nálánál kisebb osztóinak összege
 megegyezik önmagával.
 pl. 6 osztói: 1+2+3=6;
 28 osztói: 1+2+4+7+14=28.*/
+		if(args.length == 1) {
+			int szam = Integer.parseInt(args[0]);
+			int osztosum = 0;
+			
+			for (int i = 1; i < szam; i++) {
+				if(szam % i == 0) {
+					osztosum +=i;
+				}
+			}
+			if (szam == osztosum) {
+				System.out.println("A szám tökéletes!");
+			} else {
+				System.out.println("A szám nem tökéletes!");
+			}
+		} else {
+			System.out.println("Egy paramétert adj meg!");
+		}
+		
 	}
 
 }

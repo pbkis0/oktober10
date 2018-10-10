f0a21d5a4129ffc13988ecc0eab0f81abd453f9e
Balázs Kis <kis.balazs2014@outlook.com>
2018-10-10 18:40:56 +0200

    masodikFeladat kesz

--- oktober10/src/oktober10/elsoFeladat.java
+++ oktober10/src/oktober10/elsoFeladat.java
@@ -4,7 +4,10 @@ public class elsoFeladat {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+/*Írj programot, amely kiírja a felhasználó által adott összes paramétert!
+Segítség: a paraméterek számát az args tömb hosszából tudjuk meg, amit
+az args.length hivatkozás ad meg. */
+		
 	}
 
 }
---
+++ oktober10/src/oktober10/masodikFeladat.java
@@ -0,0 +1,17 @@
+package oktober10;
+
+public class masodikFeladat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*Írj programot, mely kiírja egymás mellé az 1 és 20 közé eso páros számokat! */
+		
+		int i =0;
+			while (i <20) {
+				i +=2;
+				System.out.print(i+",");
+			}
+	
+	}
+
+}
